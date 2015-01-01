9fa732d9bd3abbb3ae23655fd8c31322eff1a437
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-01 16:18:15 -0300

    add spec for serving sessions template

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -1,9 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TemplatesController, :type => :controller do
-  %w[
-    home
-  ].each do |template|
+  %w[home sessions].each do |template|
     it "should serve the #{template} template" do
       get 'template', { path: template }
       expect(response).to be_success
