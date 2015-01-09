1fcd0c0a43aed39f37543593d1b403495327f9a7
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-08 22:54:17 -0300

    customize and style edit profile devise view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,5 +7,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :country, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :country, :email, :password, :password_confirmation, :current_password, :website, :bio) }
   end
 end
