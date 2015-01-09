2e9a50cade6b63690665277e1338b6a29ae7fab8
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-09 20:10:33 -0300

    style devise views for validation errors display

---
+++ app/helpers/user_helper.rb
@@ -0,0 +1,37 @@
+module UserHelper
+  def validation_error_class? attribute
+    validation_class? "error", attribute
+  end
+
+  def validation_error_details? attribute
+    validation_details? "danger", attribute
+  end
+
+  def validation_warning_class? attribute
+    validation_class? "warning", attribute
+  end
+
+  def validation_warning_details? attribute
+    validation_details? "warning", attribute
+  end
+
+  private
+  def validation_class? type, attribute
+    base_css = "form-group"
+    color_css = "has-#{type} has-feedback" if resource.errors.messages.has_key?(attribute) and resource.errors.messages[attribute].any?
+    "#{base_css} #{color_css}"
+  end
+
+  def validation_details? type, attribute
+    return "" if resource.errors.messages[attribute].empty?
+
+    message = resource.errors.messages[attribute].map {|msg| "#{attribute.to_s.camelize} #{msg}"}.join
+    icon = type == "danger" ? "glyphicon-remove" : "glyphicon-warning-sign"
+    html = <<-HTML
+      <span class="glyphicon #{icon} form-control-feedback" aria-hidden="true"></span>
+      <span class="text-#{type} small">#{message}.</span>
+    HTML
+
+    html.html_safe
+  end
+end
