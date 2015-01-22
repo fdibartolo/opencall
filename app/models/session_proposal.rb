544eba01d6d997d618f80f08848e50572df2c00a
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-22 16:31:41 -0300

    add elasticsearch custom import to index and search queries

--- app/models/session_proposal.rb
+++ app/models/session_proposal.rb
@@ -2,15 +2,56 @@ require 'elasticsearch/model'
 
 class SessionProposal < ActiveRecord::Base
   include Elasticsearch::Model
+  # include Elasticsearch::Model::Callbacks TBD!
 
   belongs_to :user
   has_many :comments
   has_and_belongs_to_many :tags
 
   def as_indexed_json options={}
-    self.as_json(
-      only: [:id, :title, :description ],
-      include: { user: { methods: [:full_name], only: [:full_name] }}
-    )
+    JSON.parse(Jbuilder.encode do |json|
+      json.id           self.id
+      json.title        self.title
+      json.description  self.description
+      json.author       self.user.full_name
+      json.tags         self.tags.map(&:name)
+    end)
+  end
+
+  def self.custom_search terms
+    query = terms.blank? ? self.match_all_query : self.aggregated_query(terms)
+    search query
+  end
+
+  def self.aggregated_query terms
+    Jbuilder.encode do |json|
+      json.query do
+        json.multi_match do
+          json.fields ["tags^10", "title^5", "description"]
+          json.query terms
+        end
+      end
+      json.highlight do
+        json.fields do
+          json.title "{}"
+          json.description "{}"
+        end
+      end
+      json.aggregations do
+        json.matched_tags do
+          json.terms do
+            json.field "tags"
+          end
+        end
+      end
+    end
+  end
+
+  def self.match_all_query
+    Jbuilder.encode do |json|
+      json.query do
+        json.match_all Hash.new
+      end
+    end
   end
 end
