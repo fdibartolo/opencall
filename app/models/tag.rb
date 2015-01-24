1d7986d7b9b8d50a9135461acc70b0da2d5c2f1a
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-24 16:48:34 -0300

    enable tag model for elasticsearch indexing

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,39 @@
 class Tag < ActiveRecord::Base
+  include Elasticsearch::Model
+
   has_and_belongs_to_many :session_proposals
 
   validates :name,  presence: true
+
+  mapping do
+    indexes :name, type: "string"
+    indexes :name_suggest, type: "completion" #, payloads: true
+  end
+
+  def name_suggest
+    self.name
+  end
+
+  def as_indexed_json options={}
+    self.as_json(
+      only: :name,
+      methods: :name_suggest
+    )
+  end
+
+  def self.search_all
+    query = Jbuilder.encode do |json|
+      json.query do
+        json.match_all Hash.new
+      end
+    end
+
+    search query
+  end
+
+  def self.suggest term
+    response = __elasticsearch__.client.suggest index: 'tags', 
+                                                body: { tags: { text: term, completion: { field: 'name_suggest' }}}
+    response['tags'][0]['options'].each{|h| h.reject! {|k,v| k == "score"}}
+  end
 end
