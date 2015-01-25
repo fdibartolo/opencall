e8c17a58dca23ae7c78bf86d424a692e581a49c1
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-25 16:22:32 -0300

    update comments schema

---
+++ db/migrate/20150125180313_add_user_ref_to_comments.rb
@@ -0,0 +1,6 @@
+class AddUserRefToComments < ActiveRecord::Migration
+  def change
+    add_reference :comments, :user, index: true
+    add_foreign_key :comments, :users
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150121162401) do
+ActiveRecord::Schema.define(version: 20150125180313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "comments", force: :cascade do |t|
-    t.string   "author"
     t.text     "body"
     t.datetime "created_at",          null: false
     t.datetime "updated_at",          null: false
     t.integer  "session_proposal_id"
+    t.integer  "user_id"
   end
+
+  add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
 
   create_table "identities", force: :cascade do |t|
     t.string   "uid"
@@ -83,5 +85,6 @@ ActiveRecord::Schema.define(version: 20150121162401) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "comments", "users"
   add_foreign_key "identities", "users"
 end
