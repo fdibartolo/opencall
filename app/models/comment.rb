100bd57ab0f36cf2f2ce02e721e030ccc244ef37
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-01-25 16:23:32 -0300

    update comment user association

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ActiveRecord::Base
   belongs_to :session_proposal
+  belongs_to :user
 end
--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -2,7 +2,8 @@ require "rails_helper"
 
 RSpec.describe Comment, :type => :model do
   describe "attributes" do
-    it { expect(Comment.attribute_names).to include 'author' }
     it { expect(Comment.attribute_names).to include 'body' }
+    it { expect(Comment.attribute_names).to include 'session_proposal_id' }
+    it { expect(Comment.attribute_names).to include 'user_id' }
   end  
 end
