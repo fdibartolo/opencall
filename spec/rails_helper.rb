f46b0e62b6a5eeddc52ec0be5348710beb67b4c3
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-02-03 20:30:26 -0300

    setup coveralls

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 require 'devise'
 require 'support/controller_macros'
+require 'coveralls'
+Coveralls.wear!('rails')
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
