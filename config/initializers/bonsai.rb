ae7229635bd7ea5c56596ea2507c796fba4515ff
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-02-07 13:24:25 -0300

    specify different elasticsearch cluster for specs

--- config/initializers/bonsai.rb
+++ config/initializers/bonsai.rb
@@ -1,1 +1,2 @@
-Elasticsearch::Model.client = Elasticsearch::Client.new url: ENV['BONSAI_URL']
+host = Rails.env.test? ? "#{ENV['ES_TEST_HOST']}:#{ENV['ES_TEST_PORT']}" : ENV['BONSAI_URL']
+Elasticsearch::Model.client = Elasticsearch::Client.new url: host
