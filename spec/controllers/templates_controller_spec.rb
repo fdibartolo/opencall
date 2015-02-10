3ab52c6ee171de6a87bb067220810e41b538eab6
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-02-09 23:36:34 -0300

    fix breaking spec

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TemplatesController, :type => :controller do
-  %w[home sessions/index sessions/new sessions/search_result].each do |template|
+  %w[home sessions/index sessions/new sessions/search_result error].each do |template|
     it "should serve the #{template} template" do
       get 'template', { path: template }
       expect(response).to be_success
