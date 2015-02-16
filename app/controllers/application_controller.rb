d2fbbbaf0393848878270896d87472495f93e9d3
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-02-16 12:49:46 -0300

    update social url on users profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:first_name, :last_name, :country, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :country, :email, :password, :password_confirmation, :current_password, :website, :bio) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :country, :email, :password, :password_confirmation, :current_password, :website, :bio, :linkedin, :aboutme, :twitter, :facebook) }
   end
 end
