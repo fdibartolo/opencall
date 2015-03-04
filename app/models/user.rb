7cc586b2b08f78c7b23472d513fb0a9884a0b597
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-03-04 17:41:01 -0300

    temporarily disable github and google oauth login

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  devise :omniauthable, :omniauth_providers => [:github, :google_oauth2, :linkedin]
+  devise :omniauthable, :omniauth_providers => [:linkedin]
 
   validates :first_name,  presence: true
   validates :last_name,   presence: true
