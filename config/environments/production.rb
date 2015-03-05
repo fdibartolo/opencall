5e2e86833021648cba290e221fa8490d16a35a62
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-03-04 21:12:28 -0300

    set host for action mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'TBD' }
+  config.action_mailer.default_url_options = { host: 'opencall.herokuapp.com' }
 end
