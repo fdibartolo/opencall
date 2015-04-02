d2da687a2ccd5e0ae1c626697d0026fddfa0f5a2
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-04-02 11:40:10 -0300

    allow update user profile without requiring its password

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -3,4 +3,8 @@ class Users::RegistrationsController < Devise::RegistrationsController
   def after_update_path_for(resource)
     edit_user_registration_path(resource)
   end
+
+  def update_resource(resource, params)
+    resource.update_without_password(params)
+  end
 end
--- spec/controllers/registrations_controller_spec.rb
+++ spec/controllers/registrations_controller_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe Users::RegistrationsController, :type => :controller do
   login_as :user
 
   describe "PATCH update" do
-    it "should stay in user profile page" do
-      expect(patch :update).to render_template(:edit)
+    let(:payload) { { :user => { :bio => 'updated bio' } } }
+
+    before :each do
+      patch :update, payload
     end
+
+    it { expect(response.location).to match edit_user_registration_path }
+    it { expect(logged_in(:user).bio).to eq 'updated bio' }
   end
 end
