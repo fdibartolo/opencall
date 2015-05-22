a61548478fecef81a557ab33ca7fd8152900ae5c
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-05-22 16:23:31 -0300

    add template spec

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -3,8 +3,8 @@ require 'rails_helper'
 RSpec.describe TemplatesController, :type => :controller do
   %w[home error loading_spinner stats
     sessions/index sessions/new sessions/search_result sessions/search_side_bar sessions/review
-    sessions/comments sessions/actions sessions/reviews_summary users/faved_sessions users/reviews 
-    users/sessions users/voted_sessions users/profile_summary
+    sessions/comments sessions/actions sessions/reviews_summary sessions/show users/faved_sessions
+    users/reviews users/sessions users/voted_sessions users/profile_summary
   ].each do |template|
     it "should serve the #{template} template" do
       get 'template', { path: template }
