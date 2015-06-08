b87699c76974241d7c84b78e6ae8d97db99bfe51
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-06-08 11:49:23 -0300

    support named loggedin roles

--- spec/support/controller_helpers.rb
+++ spec/support/controller_helpers.rb
@@ -1,6 +1,6 @@
 module ControllerHelpers
-  def logged_in type
-    user = FactoryGirl.build(type)
+  def logged_in type, first_name=''
+    user = first_name.empty? ? FactoryGirl.build(type) : FactoryGirl.build(type, first_name: first_name)
     User.find_by(email: user.email)
   end
 end
