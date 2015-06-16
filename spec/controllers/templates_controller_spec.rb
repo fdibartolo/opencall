383404af8fd2208639bbc4261d940adee411d7a4
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-06-16 19:35:42 -0300

    move view, add and update template specs accordingly

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe TemplatesController, :type => :controller do
-  %w[home error loading_spinner stats themes_list
+  %w[home error loading_spinner stats themes_list notifications
     sessions/index sessions/new sessions/search_result sessions/search_side_bar sessions/review
     sessions/comments sessions/actions sessions/reviews_summary sessions/show users/faved_sessions
     users/reviews users/sessions users/voted_sessions users/profile_summary
+    reports/reviewer_comments reports/reviews_status
   ].each do |template|
     it "should serve the #{template} template" do
       get 'template', { path: template }
