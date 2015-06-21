5d75a58249d9c751806043e83cb02a837f074bf4
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2015-06-20 22:10:10 -0300

    regroup notification views and routes

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -1,11 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe TemplatesController, :type => :controller do
-  %w[home error loading_spinner stats themes_list notifications
+  %w[home error loading_spinner stats themes_list
     sessions/index sessions/new sessions/search_result sessions/search_side_bar sessions/review
-    sessions/comments sessions/actions sessions/reviews_summary sessions/show users/faved_sessions
-    users/reviews users/sessions users/voted_sessions users/profile_summary
+    sessions/comments sessions/actions sessions/reviews_summary sessions/show
+    users/faved_sessions users/reviews users/sessions users/voted_sessions users/profile_summary
     reports/reviewer_comments reports/reviews_status
+    notifications/acceptance notifications/author_inbox
   ].each do |template|
     it "should serve the #{template} template" do
       get 'template', { path: template }
