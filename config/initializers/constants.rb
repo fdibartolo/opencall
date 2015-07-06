2894fba675d5829197b14f2e258ecbad9bc45cc8
NicoPaez <nicopaez@gmail.com>
2015-07-05 21:52:49 -0300

    Duedate set to July7

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -8,7 +8,7 @@ MaxSessionProposalVotes = 10
 MailerAccount = "Sesiones Agiles 2015 <sesiones@agiles.org>"
 ChairsAccount = "sesiones2015@agiles.org"
 
-SubmissionDueDate = Date.parse "July 6 2015" # as is until we update the system to support multi-event
+SubmissionDueDate = Date.parse "July 7 2015" # as is until we update the system to support multi-event
 AcceptanceDueDate = DateTime.parse "August 17 2015 23:59 UTC-3" # as is until we update the system to support multi-event
 
 Version = "1.0.11"
