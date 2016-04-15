6b0ef0c04f8d0b93da54886a84bf058ff5c09555
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2016-04-15 11:32:25 -0300

    create indices if needed upon bootstraping

--- config/initializers/searchbox.rb
+++ config/initializers/searchbox.rb
@@ -1,2 +1,13 @@
 host = Rails.env.test? ? "#{ENV['ES_TEST_HOST']}:#{ENV['ES_TEST_PORT']}" : ENV['SEARCHBOX_URL']
 Elasticsearch::Model.client = Elasticsearch::Client.new url: host
+
+# create indices if they dont exist
+Process.fork do
+  sleep(10) # hack :(
+  [SessionProposal, Tag].each do |model|
+    unless Elasticsearch::Model.client.indices.exists?(index: model.index_name)
+      p "creating index #{model.index_name}..."
+      model.__elasticsearch__.create_index!
+    end
+  end
+end
