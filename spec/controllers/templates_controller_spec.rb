dcbd2ffa1b9e45098d5217c9e30aa29dc88388d2
Fernando Di Bartolo <fernando.di.bartolo@gmail.com>
2016-04-23 16:40:23 -0300

    refactor card into its own view

--- spec/controllers/templates_controller_spec.rb
+++ spec/controllers/templates_controller_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe TemplatesController, :type => :controller do
 
   %w[home error loading_spinner stats themes_list
     sessions/index sessions/new sessions/search_result sessions/search_side_bar sessions/review
-    sessions/comments sessions/actions sessions/reviews_summary sessions/show
+    sessions/comments sessions/actions sessions/reviews_summary sessions/show sessions/card
     users/faved_sessions users/reviews users/sessions users/voted_sessions users/profile_summary
     reports/reviewer_comments reports/reviews_status reports/session_votes.html.erb
     notifications/acceptance notifications/author_inbox
