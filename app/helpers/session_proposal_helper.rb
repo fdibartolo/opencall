86806e70fa691ea2ae549d59e24b6349c0a817e1
federicojsequeira <federicojsequeira@gmail.com>
2016-06-26 11:22:20 -0300

    Fix #111: fix indentation over session_proposal_helper.rb

--- app/helpers/session_proposal_helper.rb
+++ app/helpers/session_proposal_helper.rb
@@ -3,7 +3,7 @@ module SessionProposalHelper
 		current_user_is_author?(current_user_id, author_id) ? t('sessions.placeholders.author_comment') : t('sessions.placeholders.public_comment')
 	end
 
-	private 
+	private
 	def current_user_is_author? current_user_id, author_id
 		current_user_id == author_id
 	end
